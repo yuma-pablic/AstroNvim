27e97ad826e3459e5bf697d71564fe2f7d5df1b7
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-08-30 23:57:21 +0900

    feat

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -72,6 +72,11 @@ return {
             )
           end,
           desc = "Close buffer from tabline",
+        },
+
+        i = {
+          ["jj"] = { "<esc>", desc = "Exit insert mode" },
+          ["kk"] = { "<esc>", desc = "Exit insert mode" },
         },
 
         -- tables with just a `desc` key will be registered with which-key if it's installed
