9beac37540b9858680940d10f871fb4658f81a46
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-09-23 16:19:57 +0900

    feat

--- lua/plugins/comment-box.lua
+++ lua/plugins/comment-box.lua
@@ -1,3 +1,11 @@
 return {
   "LudoPinelli/comment-box.nvim",
+  keys = {
+    -- Normal and Visual mode keymaps
+    { "<Leader>cb", "<Cmd>CBccbox<CR>", mode = { "n", "v" }, desc = "Comment box title" },
+    { "<Leader>ct", "<Cmd>CBllline<CR>", mode = { "n", "v" }, desc = "Comment box named part" },
+    { "<Leader>cl", "<Cmd>CBline<CR>", mode = "n", desc = "Comment box simple line" },
+    { "<Leader>cm", "<Cmd>CBllbox14<CR>", mode = { "n", "v" }, desc = "Comment box marked" },
+    { "<Leader>cd", "<Cmd>CBd<CR>", mode = { "n", "v" }, desc = "Remove comment box" },
+  },
 }
