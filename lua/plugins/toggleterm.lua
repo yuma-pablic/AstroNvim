db3d832c64607c9c1fe6272484235b9f84e0bd43
yuma-pablic <yuumanagai0504pablic@gmail.com>
2025-11-02 13:30:28 +0900

    feat

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -23,22 +23,20 @@ return {
   },
   config = function()
     require("toggleterm").setup({
-      -- size を関数にして、方向に応じて動的にサイズを決定
+      open_mapping = [[<c-t>]],
+      hide_numbers = true,
+      shade_terminals = true,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "horizontal", -- デフォルトは横分割
       size = function(term)
         if term.direction == "horizontal" then
           return math.floor(vim.o.lines * 0.5)
         elseif term.direction == "vertical" then
           return math.floor(vim.o.columns * 0.5)
         end
-        return 20
       end,
-      open_mapping = [[<c-t>]],
-      hide_numbers = true,
-      shade_terminals = true,
-      start_in_insert = true,
-      insert_mappings = true,
-      persist_size = false,
-      direction = "horizontal", -- デフォルトは横分割
       close_on_exit = true,
       shell = vim.o.shell,
     })
